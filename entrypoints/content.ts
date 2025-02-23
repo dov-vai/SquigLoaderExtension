89d189b12de75fe11af2557c6e557a0ede145a40
dov-vai <160327869+dov-vai@users.noreply.github.com>
2025-02-23 19:57:34 +0200

    fix: support for chrome when handling phoneListItem is null error

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -166,7 +166,10 @@ export default defineContentScript({
             showPhone(p, exclusive, suppressVariant, trigger);
         } catch (error) {
             // ignore this error, showPhone handles list view updating too, however it's not needed for us and causes this error
-            if (error instanceof TypeError && error.message.includes("phoneListItem is null")) {
+            if (error instanceof TypeError 
+                && (error.message.includes("phoneListItem is null")
+                || error.message.includes("Cannot read properties of null"))
+            ) {
                 console.warn("Ignoring TypeError: phoneListItem is null");
             } else {
                 throw error;
